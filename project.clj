b91c3cac5509f10999895b1842ec7a501b957a22
Darrick Wiebe <dw@xnlogic.com>
2016-03-27 20:51:53 -0400

    Make core.match a full dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/test.check "0.9.0"]
-                 [org.clojure/math.combinatorics "0.1.1"]]
-  :profiles
-  {:dev
-   {:dependencies [[org.clojure/core.match "0.3.0-alpha4"]]}})
+                 [org.clojure/math.combinatorics "0.1.1"]
+                 [org.clojure/core.match "0.3.0-alpha4"]])
