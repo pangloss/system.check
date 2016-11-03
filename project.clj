c709926df9da4a2cbf95784cdd3238b026051cfc
Darrick Wiebe <dw@xnlogic.com>
2016-11-03 01:03:17 -0400

    deploy repo

--- project.clj
+++ project.clj
@@ -5,4 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/test.check "0.9.0"]
                  [org.clojure/math.combinatorics "0.1.3"]
-                 [org.clojure/core.match "0.3.0-alpha4"]])
+                 [org.clojure/core.match "0.3.0-alpha4"]]
+  :deploy-repositories
+  [["releases"
+    {:url "http://repo.macroscale.net/artifactory/libs-release-local" }]
+   ["snapshots"
+    {:url "http://repo.macroscale.net/artifactory/libs-snapshot-local" }]])
