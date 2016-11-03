a1334ac57f35ea6c159c4b0a47599212b99399f6
Darrick Wiebe <dw@xnlogic.com>
2016-11-03 00:56:04 -0400

    Prepare release

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject macroscale/system.check "0.1.0-SNAPSHOT"
+(defproject macroscale/system.check "0.1.0"
   :description "Plug simulation capability in to test.check"
-  :url "https://macroscale.com/open-source"
+  :url "https://macroscale.net/open-source"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/test.check "0.9.0"]
-                 [org.clojure/math.combinatorics "0.1.1"]
+                 [org.clojure/math.combinatorics "0.1.3"]
                  [org.clojure/core.match "0.3.0-alpha4"]])
